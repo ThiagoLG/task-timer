02b8b3b680f2b78ae49dcc48000028281aa585c4
ThiagoLG <thiago.lg53@gmail.com>
2023-03-09 01:00:31 +0000

    configured routes and base layout to use in pages, like as template

--- task-timer/src/App.tsx
+++ task-timer/src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div className="App">
-        <h1>Teste</h1>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
