44c781d06b46edfb8ede7aca80297f9d435ad481
ThiagoLG <thiago.lg53@gmail.com>
2023-03-10 01:20:29 -0300

    implementing header component

--- task-timer/src/components/Header/index.tsx
+++ task-timer/src/components/Header/index.tsx
@@ -1,5 +1,19 @@
 import { HeaderContainer } from './styles'
+import logo from '../../assets/logo.svg'
+import { Timer, Scroll } from 'phosphor-react'
 
 export function Header() {
-  return <HeaderContainer>Header</HeaderContainer>
+  return (
+    <HeaderContainer>
+      <img src={logo} alt="Site logo" />
+      <nav>
+        <a href="">
+          <Timer size={24} />
+        </a>
+        <a href="">
+          <Scroll size={24} />
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
 }
